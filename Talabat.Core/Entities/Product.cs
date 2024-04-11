8a78ff589bcc81c80fe2d31a83caa7d15163e437
HP <HP@DESKTOP-GR1U1GH>
2024-04-11 16:50:48 +0200

    ProductModule-Entities

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	internal class Product : BaseEntity
+	{
+        public String Name { get; set; }
+        public string Discription { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public int BrandId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int CategoryId { get; set; }
+        public ProductCategory ProductCategory { get; set; }
+    }
+}
