4753d4c3fc34d457e332bb2a51c31140cb7b39e3
HP <HP@DESKTOP-GR1U1GH>
2024-04-11 20:29:51 +0200

    UpdateDataBase

--- Route.Talabat.Api/Program.cs
+++ Route.Talabat.Api/Program.cs
@@ -1,12 +1,13 @@
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
 using Talabat.Infrastructure.Data;
 
 namespace Route.Talabat.Api
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
@@ -14,13 +15,27 @@ namespace Route.Talabat.Api
 			// Add services to the container.
 
 			builder.Services.AddControllers();
-			builder.Services.AddDbContext<StoreContext>(options =>{ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConntection"));});
+			builder.Services.AddDbContext<StoreContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConntection")); });
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen(); 
+			builder.Services.AddSwaggerGen();
 			#endregion
 
 			var app = builder.Build();
+
+			using var scope = app.Services.CreateScope(); 
+			var services = scope.ServiceProvider;
+			var LoggerFactory = services.GetRequiredService<ILoggerFactory>();
+			try
+			{
+				var dbContext = services.GetRequiredService<StoreContext>();
+				await dbContext.Database.MigrateAsync();
+			}
+			catch (Exception ex)
+			{
+				var logger = LoggerFactory.CreateLogger<Program>();
+				logger.LogError(ex, "an errorr occured during apply the migration");
+			}
 
 			#region Add Kesteral Middllwears
 			// Configure the HTTP request pipeline.
@@ -35,7 +50,7 @@ namespace Route.Talabat.Api
 			//app.UseAuthorization();
 
 
-			app.MapControllers(); 
+			app.MapControllers();
 			#endregion
 
 			app.Run();
