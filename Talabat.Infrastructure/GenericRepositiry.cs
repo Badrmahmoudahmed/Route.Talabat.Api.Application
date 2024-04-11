2ad2a8c81ba8dfd852d417d8f9d2147362b02987
HP <HP@DESKTOP-GR1U1GH>
2024-04-11 21:53:58 +0200

    GenericRepositry

---
+++ Talabat.Infrastructure/GenericRepositiry.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositiry.Contract;
+using Talabat.Infrastructure.Data;
+
+namespace Talabat.Infrastructure
+{
+	public class GenericRepositiry<T> : IGenericRepositiryy<T> where T : BaseEntity
+	{
+
+		private readonly StoreContext _dbContext;
+		public GenericRepositiry(StoreContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+		public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			return await _dbContext.Set<T>().ToListAsync();
+		}
+		public async Task<T?> GetByIdAsync(int id)
+		{
+			return await _dbContext.Set<T>().FindAsync(id);
+		}
+	} 
+}
