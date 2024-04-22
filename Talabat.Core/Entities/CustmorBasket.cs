76f862da5e7b58af6dbc00749a8d087c615110aa
HP <HP@DESKTOP-GR1U1GH>
2024-04-22 21:42:26 +0200

    BasketEntities

---
+++ Talabat.Core/Entities/CustmorBasket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	internal class CustmorBasket
+	{
+		public string Id { get; set; }
+        public List<BasketItem> BasketItems { get; set; }
+    }
+
+
+}
