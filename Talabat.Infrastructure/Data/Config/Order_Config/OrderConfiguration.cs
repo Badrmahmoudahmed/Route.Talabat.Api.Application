cd3d21f9b50b1d1e1e7866c7880d424a033f5c58
HP <HP@DESKTOP-GR1U1GH>
2024-05-05 22:31:48 +0300

    OrderModuleConfiguration

---
+++ Talabat.Infrastructure/Data/Config/Order_Config/OrderConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.OredrAggregate;
+
+namespace Talabat.Infrastructure.Data.Config.Order_Config
+{
+	internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+	{
+		public void Configure(EntityTypeBuilder<Order> builder)
+		{
+			builder.OwnsOne(o => o.ShippingAdress, o => o.WithOwner());
+			builder.Property(o => o.Statues).HasConversion(
+					(o) => o.ToString(),
+					(o) => (OrderStatues)Enum.Parse(typeof(OrderStatues), o)
+				);
+
+			builder.Property(o => o.SubTotal).HasColumnType("decimal(12,2)");
+		}
+	}
+}
---
+++ Talabat.Infrastructure/Data/Config/Order_Config/OrderItemConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.OredrAggregate;
+
+namespace Talabat.Infrastructure.Data.Config.Order_Config
+{
+	public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+	{
+		public void Configure(EntityTypeBuilder<OrderItem> builder)
+		{
+			builder.OwnsOne(OI => OI.Product, o => o.WithOwner());
+			builder.Property(OI => OI.Price).HasColumnType("decimal(12,2)");
+		}
+	}
+}
