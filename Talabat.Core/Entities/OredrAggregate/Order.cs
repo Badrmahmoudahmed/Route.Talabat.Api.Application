41fea8d8119a9b4d3449e080c046add8e12318c9
HP <HP@DESKTOP-GR1U1GH>
2024-05-05 15:05:29 +0300

    OrderModule

---
+++ Talabat.Core/Entities/OredrAggregate/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.OredrAggregate
+{
+	public class Order : BaseEntity
+	{
+        public string BuyerEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+        public OrderStatues Statues { get; set; } = OrderStatues.Pending;
+        public Adress ShippingAdress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+        public decimal SubTotal { get; set; }
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Cost;
+
+        public string PaymetnIntentId { get; set; } = string.Empty;
+    }
+}
