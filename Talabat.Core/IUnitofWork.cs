68aada19c6bc7bab6b5b585aae80c37b6835e0c8
HP <HP@DESKTOP-GR1U1GH>
2024-05-06 22:53:25 +0300

    UnitofWork

---
+++ Talabat.Core/IUnitofWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.OredrAggregate;
+using Talabat.Core.Repositiry.Contract;
+
+namespace Talabat.Core
+{
+	public interface IUnitofWork : IAsyncDisposable
+	{
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+
+        Task<int> CompleteAsync();
+    }
+}
---
+++ Talabat.Infrastructure/UnitofWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.OredrAggregate;
+using Talabat.Core.Repositiry.Contract;
+using Talabat.Infrastructure.Data;
+
+namespace Talabat.Infrastructure
+{
+	public class UnitofWork : IUnitofWork
+	{
+		private readonly StoreContext _dbcontext;
+
+        public Hashtable _repositiory { get; set; }
+
+        public UnitofWork(StoreContext dbcontext)
+        {
+			_dbcontext = dbcontext;
+			_repositiory = new Hashtable();
+		}
+        public async Task<int> CompleteAsync()
+		{
+			return await _dbcontext.SaveChangesAsync();
+		}
+
+		public async ValueTask DisposeAsync()
+		{
+			await _dbcontext.DisposeAsync();
+		}
+
+		public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+		{
+			var key = typeof(TEntity).Name;
+			if(!_repositiory.ContainsKey(key))
+			{
+				var repositiory = new GenericRepositiry<TEntity>(_dbcontext);
+				_repositiory.Add(key, repositiory);
+			}
+
+			return _repositiory[key] as IGenericRepository<TEntity>;
+		}
+
+	
+	}
+}
